07d83f4f4c8c5a322ccfade450be56eb0fa92700
aaron_yu <yuwy412@hotmail.com>
2021-07-17 17:42:56 +0800

    新建了javascript.js文件

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let a = 6;
+let b = 9;
+
+function simpleTag(strings, aValExpression, bVaExpression, sumExpreeion){
+    console.log(strings);
+    console.log(aValExpression);
+    console.log(bVaExpression);
+    console.log(sumExpreeion);
+
+    return 'foobar';
+}
+
+let untaggedResult = `${a} +${b} = ${a + b}`;
+let taggedResult = simpleTag`${a} +${b} = ${a + b}`;
